dcc685d83b734cd5354432a1b3263a69305d8cf4
Adam Van Hine <45979414+vanhine@users.noreply.github.com>
2020-09-03 23:54:37 -0700

    Still working on testing

--- app/src/test/java/com/mrwinston/guitarburst/viewmodel/PiecesViewModelTest.kt
+++ app/src/test/java/com/mrwinston/guitarburst/viewmodel/PiecesViewModelTest.kt
@@ -2,25 +2,47 @@ package com.mrwinston.guitarburst.viewmodel
 
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import com.google.common.truth.Truth.assertThat
 import com.mrwinston.guitarburst.data.PiecesRepository
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.spy
-import org.junit.Test
-import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import java.util.concurrent.Executors
 
-
+@kotlinx.coroutines.ExperimentalCoroutinesApi
 class PiecesViewModelTest {
 
-    @get:Rule val rule = InstantTaskExecutorRule()
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+
+    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
+    private val mainThreadSurrogate = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
 
     @Test
-    fun searchPieces_sets_isLoading() {
+    fun searchPieces_sets_isLoading() = testCoroutineScope.runBlockingTest {
         val mockPiecesRepository: PiecesRepository = mock()
         val mockApplication: Application = mock()
         val viewModel = PiecesViewModel(mockPiecesRepository, mockApplication)
-        viewModel.isLoading.observeForever {  }
+        viewModel.isLoading.observeForever { }
+        viewModel.searchPieces("Adam")
+        assertThat(viewModel.isLoading.value).isFalse()
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
     }
 }
