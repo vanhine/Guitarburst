aa565732d5f7b5d98dd700f743497743df677937
Adam Van Hine <45979414+vanhine@users.noreply.github.com>
2020-09-13 23:04:25 -0700

    More Slider theming

--- app/src/main/java/com/mrwinston/guitarburst/view/FilterFragment.kt
+++ app/src/main/java/com/mrwinston/guitarburst/view/FilterFragment.kt
@@ -1,14 +1,15 @@
 package com.mrwinston.guitarburst.view
 
 import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.slider.RangeSlider
 import com.mrwinston.guitarburst.R
 import com.mrwinston.guitarburst.databinding.FilterFragmentBinding
 import com.mrwinston.guitarburst.viewmodel.PiecesViewModel
@@ -33,21 +34,52 @@ class FilterFragment : Fragment(R.layout.filter_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupEraDropdown()
-        binding.difficultySlider.addOnChangeListener { slider, value, fromUser ->
-            when (value.toInt()) {
-                in 0..5 -> {
-                    slider.thumbTintList = ColorStateList.valueOf(Color.GREEN)
-                    slider.trackTintList = ColorStateList.valueOf(Color.GREEN)
-                    slider.tickTintList = ColorStateList.valueOf(Color.GREEN)
-                }
-                in 5..10 -> {
-                    slider.thumbTintList = ColorStateList.valueOf(Color.YELLOW)
-                    slider.trackTintList = ColorStateList.valueOf(Color.YELLOW)
-                    slider.tickTintList = ColorStateList.valueOf(Color.YELLOW)
-                }
+        setupDifficultySlider()
+    }
 
+    private fun setupDifficultySlider() {
+        binding.difficultySlider.values = mutableListOf(1F, 5F)
+        binding.difficultySlider.addOnChangeListener { slider, value, fromUser ->
+            var primaryColor = 0
+            var secondaryColor = 0
+            var labelString = ""
+            when (value.toInt()) {
+                in 1..5 -> { // Beginner
+                    primaryColor = getColor(R.color.easy_difficulty_primary)
+                    secondaryColor = getColor(R.color.easy_difficulty_secondary)
+                    labelString = "Beginner"
+                }
+                in 6..10 -> { // Intermediate
+                    primaryColor = getColor(R.color.indermediate_difficulty_primary)
+                    secondaryColor = getColor(R.color.intermediate_difficulty_secondary)
+                    labelString = "Intermediate"
+                }
+                in 11..15 -> { // Advanced
+                    primaryColor = getColor(R.color.advanced_difficulty_primary)
+                    secondaryColor = getColor(R.color.advanced_difficulty_secondary)
+                    labelString = "Advanced"
+                }
+                in 16..20 -> { // Professional
+                    primaryColor = getColor(R.color.professional_difficulty_primary)
+                    secondaryColor = getColor(R.color.professional_difficulty_secondary)
+                    labelString = "Professional"
+                }
+            }
+            setSliderColors(slider, primaryColor, secondaryColor)
+            slider.setLabelFormatter {
+                "$labelString\n$value"
             }
         }
+    }
+
+    private fun setSliderColors(slider: RangeSlider, primaryColor: Int, secondaryColor: Int) {
+        slider.thumbTintList = ColorStateList.valueOf(primaryColor)
+        slider.trackTintList = ColorStateList.valueOf(primaryColor)
+        slider.trackInactiveTintList = ColorStateList.valueOf(secondaryColor)
+    }
+
+    private fun getColor(colorId: Int): Int {
+        return ContextCompat.getColor(requireContext(), colorId)
     }
 
     private fun setupEraDropdown() {
